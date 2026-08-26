3d878f4760ad7e7f8eb5404af74f2c2cfa3c93a2
Abhirup-2007 <in.abhirup@gmail.com>
2026-08-26 14:51:39 +0530

    Update page.jsx

--- frontend/src/config/page.jsx
+++ frontend/src/config/page.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://proconnect-linkdinclone.onrender.com/"
+export const BASE_URL = "https://proconnect-linkdinclone.onrender.com"
 
 const clientServer = axios.create({
   baseURL: BASE_URL,
